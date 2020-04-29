e583ce4932a93ba49a8b2989c7be4f452a41703b
ambassadordave <46604480+ambassadordave@users.noreply.github.com>
2020-04-28 23:34:21 -0400

    Added schematics, cleaned up code

--- GATECeR.ino
+++ GATECeR.ino
@@ -1,16 +1,85 @@
-// Generic Arduino Thermoelectric Cooler Regulator (GATECeR)
+// Generic Arduino-based Thermo-Electric Cooler Regulator (GATECeR)
 // 2020-04-29 - ver 1.0
 //
 // Built using:
 // Arduino Nano Every
 // Adafruit Peltier Thermo-Electric Cooler Module+Heatsink Assembly - 12V 5A (Part 1335)
-// Analog Devices Low Voltage Temperature Sensor (TMP36)
 // SparkFun MOSFET Power Control Kit (Part COM-12959) [includes Harris RFP30N06LE MOSFET]
-// 4D Systems 1.44" Display Module (uLCD-144-G2)
+// Analog Devices Low Voltage Temperature Sensor (TMP36)
 // SparkFun Trimpot 10K Ohm with Knob (Part COM-09806)
+// 4D Systems 1.44" Display Module (uLCD-144-G2)
 // Generic 12V/5A power supply unit
+// Generic 5V/1A power supply unit
 // assorted electronics (e.g. breadboard, on/off switch, power connector, wires)
 //
+
+
+//
+//  MOSFET control board (to TEC) pinout:    with flat side facing & leads pointing down,
+//
+//
+//                            +------- - ----------------> TEC black wire
+//                            |   +------ + -------------> TEC red wire  
+//                            |   |
+//                           -------
+//                           [MOSFET]
+//                           -------
+//                           |  |  |
+//                           |  |  +----- + -------------> +12V
+//                           |  +----- - ----------------> GND
+//                           +----- C -------------------> TECPin 
+//
+//
+//
+//MOSFET control board (to TEC) Pin Variables
+//
+int TECPin = 9;    // MOSFET to TEC connected to digital pin 9
+
+
+//
+//  tmp36 pinout:    with flat side facing & leads pointing down,
+//
+//                           -------
+//                           [tmp36]
+//                           -------
+//                           |  |  |
+//                           |  |  +----- Vin -----------> +5V
+//                           |  +----- Vout (0 - 5v) ----> A/D
+//                           |     |
+//                           |     |
+//                           |   =====  0.1uF capacitor, the key to accuracy
+//                           |     |
+//                           |     |
+//                           +---------------------------> GND 
+//
+//
+//
+//TMP36 Pin Variables
+//
+int sensorPin = 0;  //the analog pin the TMP36's Vout (sense) pin is connected to
+                         //the resolution is 10 mV / degree centigrade with a
+                         //500 mV offset to allow for negative temperatures
+float t36Vin = 5.0; // for tmp36 Vin of 3.3v use 3.3
+
+
+//
+//  Trimpot pinout:    horizontal orientation (which is up/down doesn't matter)
+//
+//
+//                           -------
+//                           [ POT ]
+//                           -------
+//                           |  |  |
+//                           |  |  +----- (left) --------> +5V
+//                           |  +----- (center) ---------> potPin
+//                           +----- (right) -------------> GND 
+//
+//
+//
+//Trimpot Pin Variables
+//
+int potPin = 1;    // Potentiometer on A1
+ 
 
 //
 //  uLCD pinout:    lower row of five pins along bottom edge of board,
@@ -45,57 +114,7 @@ int uLCDPin = 3;    // uLCD reset connected to pin 3 (as digital)
 #define TextColor SPRINGGREEN
 
 
-//
-//  tmp36 pinout:    with flat side facing & leads pointing down,
-//
-//                           -------
-//                           [tmp36]
-//                           -------
-//                           |  |  |
-//                           |  |  +----- Vin -----------> +5V
-//                           |  +----- Vout (0 - 5v) ----> A/D
-//                           |     |
-//                           |     |
-//                           |   =====  0.1uF capacitor, the key to accuracy
-//                           |     |
-//                           |     |
-//                           +---------------------------> GND 
-//
-//
-//
-//TMP36 Pin Variables
-//
-int sensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
-                        //the resolution is 10 mV / degree centigrade with a
-                        //500 mV offset to allow for negative temperatures
 
-
-//
-//  MOSFET control board (to TEC) pinout:    with flat side facing & leads pointing down,
-//
-//
-//                            +------- - ----------------> TEC black wire
-//                            |   +------ + -------------> TEC red wire  
-//                            |   |
-//                           -------
-//                           [MOSFET]
-//                           -------
-//                           |  |  |
-//                           |  |  +----- + -------------> +12V
-//                           |  +----- - ----------------> GND
-//                           +----- C -------------------> TECPin 
-//
-//
-//
-//MOSFET control board (to TEC) Pin Variables
-//
-
-int TECPin = 9;    // MOSFET to TEC connected to digital pin 9
-
-
-
-int potPin = 1;    // Potentiometer on A1
- 
 /*
  * setup() - this function runs once when you turn your Arduino on
  */
@@ -121,27 +140,25 @@ void setup()
   delay(100);
 }
 
-double temp_sensor_raw, temp_raw, temp_weighted;
-float scaling = 0.005; // Used to weight the impact of raw input, adjust as required
-int LoopCounter = 0;  // Counter used to track input readings
-int IsReadyToAdjust = 0;  // After 800 readings, begin any temperature adjustments (This will allow temp value to stabilise)
-int IsLoopRun = 0;  // Test for if loop first run, for first run, just set weighted = raw so that weighted doesn't start at 0
+
+
+//Global Variable Initializations
+//get_raw_temp(), maintain_temp()
+double temp_raw;
+// adjust_temp(), maintain_temp()
 float tempC, tempF;
 
-double t36val;
-float t36Vin = 5.0; // for tmp36 Vin of 3.3v use 3.3
-float voltage = 0.0;
-int PWMvalue = 70; // what PWM value to send out (0 to 255)
-float setTemp = 14.0; // what temperature (in degC) to hold at (+/- offset)
-float offset = 1.0;   // how many degC to allow +/- range around
 
 
-
+/*
+ * displayStats() - this function outputs temperature & TEC power level
+ */
 void displayStats(double tempSetIn, float cIn, float fIn, int PWMValIn)
 {
   // update stats on the uLCD screen
 
   // display the temperature values
+  // (some not very pretty code as the Display 
   Display.gfx_MoveTo(1,10);
   Display.print("Temperatures");
   // display the set temperature point
@@ -157,20 +174,29 @@ void displayStats(double tempSetIn, float cIn, float fIn, int PWMValIn)
 
   // display the output PWM level (0-255)
   Display.gfx_MoveTo(1,90);
-  Display.print("PWM value: "); Display.print(PWMValIn);
+  if (PWMValIn >= 100) {
+    Display.print("PWM value: "); Display.print(PWMValIn);    //adjust spacing for 3 digits
+  } else if (PWMValIn >= 10) {
+    Display.print("PWM value:  "); Display.print(PWMValIn);   //adjust spacing for 2 digits
+  } else {
+    Display.print("PWM value:   "); Display.print(PWMValIn);  //adjust spacing for 1 digit
+  }
+  
   delay(100);
 }
 
 
 
+/*
+ * computePID() - this function runs the proportional, integral, derivative (PID) control
+ */
 //PID Control Loop Variable Initializations
 unsigned long currentTime, previousTime;
-double elapsedTime;
-// Current error[k], Error[k-1], Cumulative Error, Error Rate
-double e_k, e_k_minus_1, e_cm, e_r;
-// Control output, control output[k-1], setpoint
+double elapsedTime;         // Current error[k], Error[k-1], Cumulative Error, Error Rate
+double e_k, e_k_minus_1, e_cm, e_r;      // Control output, control output[k-1], setpoint
 double u_k, u_k_plus_1, setPoint;
-//Constants (TUNE THESE!) Negative gains because positive error (temp too high) means increase control output (make it colder)
+//PID Constants (TUNE THESE!) Negative gains because positive error (temp too high)
+   // means increase control output (make it colder)
 double kp = -100;
 double ki = -0.001;
 double kd = -0.01; 
@@ -199,6 +225,9 @@ double computePID(double inp) {
 
 
 
+/*
+ * adjust_temp() - this function gets the temp target & sets the TEC power output level
+ */
 void adjust_temp()
 {
   // get potentiometer reading
@@ -221,26 +250,46 @@ void adjust_temp()
 
 
 
+/*
+ * get_raw_temp() - this function gets a temp reading and converts it to voltage
+ */
+//Get Raw Temp Variable Initializations
+double temp_sensor_raw;
+
 void get_raw_temp()
 {
-  temp_sensor_raw = analogRead(sensorPin);
-  temp_raw = temp_sensor_raw * t36Vin /1024.0; // should it be (1023) since 0 - 1023
+  temp_sensor_raw = analogRead(sensorPin);      // read the analog value from the TMP36
+  temp_raw = temp_sensor_raw * t36Vin / 1024.0; // convert the TMP36 value to voltage
 }
 
 
 
-double get_temp()
+/*
+ * maintain_temp() - this function averages temp readings & starts adjust_temp()
+ */
+//Maintain Temp Variable Initializations
+int IsLoopRun = 0;        // Test for if loop first run, for first run, just set 
+                              // weighted = raw so that weighted doesn't start at 0
+double temp_weighted;     // Weighted temp
+float scaling = 0.005;    // Used to weight the impact of raw input, adjust as required
+int LoopCounter = 0;      // Counter used to track input readings
+int IsReadyToAdjust = 0;  // After 800 readings, begin any temperature adjustments 
+                              // (This will allow temp value to stabilise)
+
+double maintain_temp()
 {
   get_raw_temp(); // Get raw temp
-  if(IsLoopRun == 0)  // First loop
+  
+  if(IsLoopRun == 0)  // Check if initial run / first loop and account for if so
   {
     temp_weighted = temp_raw; // no weighting applied, so that we don't start at 0;
     IsLoopRun = 1;
-    Serial.println("First loop done.");
+    Display.println("First loop done.");
     return temp_weighted;
   }
-
-  temp_weighted = temp_weighted + (temp_raw - temp_weighted) * scaling;   // scale the raw input and add/subtract to weighted (voltage)
+  
+  // scale the raw input and add/subtract to weighted (voltage) 
+  temp_weighted = temp_weighted + (temp_raw - temp_weighted) * scaling;
   tempC = (temp_weighted - 0.5) * 100;    
   // now convert to Fahrenheit
   tempF = (tempC * 9.0 / 5.0) + 32.0;
@@ -248,33 +297,39 @@ double get_temp()
   LoopCounter++;
   if(LoopCounter % 20 == 0)   // Make adjustments and print values every 20 readings
   {
-    //Serial.print(setTemp); Serial.print(" setpoint, ");
-    //Serial.print(tempC); Serial.println(" C, ");
-    //Serial.print(tempF); Serial.println("  F");
-
-    if(IsReadyToAdjust==1)
+    // display the current loop (0-800) during temperature stabilization phase
+    if(IsReadyToAdjust==0) 
     {
+      Display.gfx_MoveTo(1,23);
+      Display.print("Loop [800]: "); Display.print(LoopCounter); // display current loop
+    } else {
       adjust_temp();
     }
   }
 
   delay(10); // wait 10ms between each reading
 
-  if(LoopCounter*10==8000)  // Let temperature stabilise before making adjustments
+  if(LoopCounter==800)  // Let temperature stabilise before making adjustments
   {
-    IsReadyToAdjust=1;
-    Serial.println("Begin adjustments");
+    IsReadyToAdjust=1;  // Begin making adjustments after next loop
+    Display.println("");
+    Display.println("");
+    Display.println("Begin adjustments");
+    Display.println("");
+    Display.println("Have an ICE day!");
+    delay(2000);
+    Display.gfx_Cls();         // clear the screen of initialization items
   }
   return temp_weighted;
 }
 
 
- 
+
+/*
+ * loop() - this function loops continuously after setup() to run other functions
+ */
 void loop()          
 {
-  
-  get_temp();
-  
-  delay(10);                                     //waiting 10 milliseconds
-  
+  maintain_temp();
+  delay(10);                //wait 10 milliseconds
 }
